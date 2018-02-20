bede0bdf4bb94c06ffe4ff7d3647dde5cfbe4cf1
Federico Jordán <federicojordan@MacBook-Pro-de-Federico-Jordan.local>
2018-02-19 23:26:29 -0300

    removed extra methods

--- SRPExample/SRPExample/ItemsViewController.swift
+++ SRPExample/SRPExample/ItemsViewController.swift
@@ -25,6 +25,7 @@ class ItemsViewController: UIViewController {
 
 // MARK:- UICollectionViewDataSource
 extension ItemsViewController: UICollectionViewDataSource {
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return itemsInteractor.itemsCount()
     }
@@ -47,22 +48,8 @@ extension ItemsViewController: UICollectionViewDelegateFlowLayout {
         return cellSize
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 0.0
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 0.0
-    }
-    
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         itemsInteractor.didSelectItem(atIndex: indexPath.row)
     }
     
 }
-
-
